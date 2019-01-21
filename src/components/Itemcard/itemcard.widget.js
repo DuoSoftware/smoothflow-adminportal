00877e8ae5a01296cab3c4aeeabf23ff19fbe49c
KasunW <kasun.w@duosoftware.com>
2019-01-21 12:21:14 +0530

    Update itemcard.widget.js

--- src/components/Itemcard/itemcard.widget.js
+++ src/components/Itemcard/itemcard.widget.js
@@ -6,7 +6,9 @@ import Tag from '../Tag/tag.widget'
 import TableTwoCol from '../Table - Two Col/table_two_col.widget';
 import TagBlock from '../Tag/tagblock.widget';
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import {Button, Block} from "../common";
+import {Button, Block, Textbox} from "../common";
+import Wrap from "../../_base/_wrap";
+import Moment from 'react-moment';
 
 class ItemCard extends Component {
     constructor(props) {
@@ -14,39 +16,27 @@ class ItemCard extends Component {
     };
     render() {
         return (
-                <div className="sf-item-card-wrap">
-                    <Link to={{ pathname: (this.props.item.type === 'activity' ? '/activities/' : '/integrations/') + this.props.item.name , activity: {...this.props.item}, advanced: this.props.advanced }} className={`sf-item-card sf-border-box sf-item-card-clickable${ this.props.className ? ' ' + this.props.className : null}`}>
-                        <div className="sf-item-card-header bordered">
-                            <h3>{ this.props.item.name }</h3>
-                            <span className={`sf-activity-state sf-activity-${this.props.item.state}`}>{ this.props.item.state }</span>
-                        </div>
-                        <div className="sf-item-card-body">
-                            <div className="sf-image-text-container" style={{overflow:'hidden'}}>
-                                <div className="sf-img sf-img-m">
-                                    <img src={ this.props.item.image } alt=""/>
-                                </div>
-                                <div className="sf-text sf-flex-1">
-                                    <p>{ this.props.item.description }</p>
-                                </div>
-                            </div>
-
-                            {/*<TableTwoCol tabledata={this.props.item.pricings} />*/}
-                            {/* <UMInfo text="Free for customers viewing and creating tickets" /> */}
-
-                            <Block />
-                            <div style={{height: '27px', overflow: 'hidden'}}>
-                                <TagBlock tags={ this.props.item.tags } />
+            <div className="sf-item-card-wrap" {...this.props}>
+                <Link to={{ pathname: '/activity/' + this.props.item._id , activity: {...this.props.item}, advanced: true }} className={`sf-item-card sf-border-box sf-item-card-clickable${ this.props.className ? ' ' + this.props.className : null}`}>
+                    <div className="sf-item-card-header bordered">
+                        <h3>{ this.props.item.name }</h3>
+                        <span className="sf-activity-state">{ this.props.item.state }</span>
+                    </div>
+                    <div className="sf-item-card-body">
+                        <div className="sf-image-text-container" style={{overflow:'hidden'}}>
+                            <div className="sf-text sf-flex-1">
+                                <p>{ this.props.item.description }</p>
                             </div>
                         </div>
-                        {
-                            !this.props.user.is_logged_in
-                            ?   <div className="sf-item-card-footer">
-                                    <Button className="sf-button sf-button-primary sf-button-primary-p sf-button-block">Buy</Button>
-                                </div>
-                            :   null
-                        }
-                    </Link>
-                </div>
+                        <Textbox icon="face" mat={true} size="17">
+                            <span>{ this.props.item.tenant_name }</span>
+                        </Textbox>
+                        <Textbox icon="today" mat={true} size="17">
+                            <span><i>Created : </i><Moment format={'D MMM YYYY'}>{ this.props.item.created_date }</Moment></span>
+                        </Textbox>
+                    </div>
+                </Link>
+            </div>
         )
     }
 }
